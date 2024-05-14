a3bf64dd507870aac1ab61e448548fe5bc928898
HarshalAtre <113234343+HarshalAtre@users.noreply.github.com>
2024-05-14 15:32:13 +0530

    cards-slider

--- client/spider/src/App.js
+++ client/spider/src/App.js
@@ -1,11 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import Cards from './Components/Cards';
 
+
+import Cards from './Components/Cards';
+import Navbar from './componends/navbar/navbar';
+
+const cardData = [
+  {
+    image: "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_c_m.jpg",
+    title: "Biozyme Performance Whey",
+    description: "4.4 lb Rich chocolate",
+    price: 400,
+    originalPrice: 500,
+    discount: 100
+  },
+  {
+      image: "https://img2.hkrtcdn.com/29736/prd_2973551-MuscleBlaze-Super-Gainer-XXL-Weight-Gainer-11-lb-Chocolate_c_m.jpg",
+      title: "High Protein Peanut Butter,",
+      description: "1 kg Dark Chocolate Creamy",
+      price: 4000,
+      originalPrice: 5000,
+      discount: 1000
+    },{
+      image: "https://img8.hkrtcdn.com/32169/prd_3216827-MuscleBlaze-Creatine-Monohydrate-0.88-lb-Unflavoured_c_m.jpg",
+      title: "Super Gainer XXL Weight Gainer",
+      description: "11 lb Chocolate",
+      price: 4444,
+      originalPrice: 5555,
+      discount: 1000
+    },{
+      image: "https://img8.hkrtcdn.com/29896/prd_2989517-MuscleBlaze-High-Protein-Peanut-Butter-1-kg-Dark-Chocolate-Creamy_c_m.jpg",
+      title: "Creatine Monohydrate CreAMP",
+      description: "0.88 lb Unflavoured",
+      price: 400,
+      originalPrice: 500,
+      discount: 100
+    },
+]
 function App() {
   return (
     <>
-    <Cards/>
+    <Navbar />
+    <Cards cardData={cardData}/>
     </>
   );
 }
--- client/spider/src/Components/Cards.js
+++ client/spider/src/Components/Cards.js
@@ -1,88 +1,54 @@
-import React from 'react';
-import '../Styles/Cards.css';
+import React,{ Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../Styles/Cards.css";
 
-function Cards() {
-  return (
-    <div className='container'>
-        <div className='cover'>
-      <div className='cards'>
-        <div className='card'>
-          <img src="https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_c_m.jpg" alt="" />
-          <p style={{ margin: '0' }}>Biozyme Performance Whey</p>
-          <h4 style={{ margin: '0' }}>4.4 lb Rich chocolate</h4>
-          <p style={{ margin: '0' }}>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>₹400</span> 
-            <span style={{ textDecoration: 'line-through', color: 'gray', marginLeft: '7px' }}>₹500</span> 
-            <span style={{ color: 'green', fontSize: 'small', marginLeft: '7px' }}> Save ₹100</span>
-          </p>
-          <p style={{ margin: '0' }}>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>₹300</span> 
-            <span style={{ color: 'gray', marginLeft: '7px', fontWeight: 'bold' }}>with</span> 
-            <img className='logo' src="https://static1.hkrtcdn.com/mbnext/static/media/loyality/Union.svg" alt=""  />
-            <span style={{ color: '#ffbe00', fontWeight: 'bolder' }}> Premium</span>
-          </p>
-          <button className='btn'><b>Add to Cart</b></button>
-        </div>
+export default function SimpleSlider({cardData}) {
+    
 
-        {/* Copying the first card content to other cards */}
-        <div className='card'>
-          <img src="https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_c_m.jpg" alt="" />
-          <p style={{ margin: '0' }}>Biozyme Performance Whey</p>
-          <h4 style={{ margin: '0' }}>4.4 lb Rich chocolate</h4>
-          <p style={{ margin: '0' }}>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>₹400</span> 
-            <span style={{ textDecoration: 'line-through', color: 'gray', marginLeft: '7px' }}>₹500</span> 
-            <span style={{ color: 'green', fontSize: 'small', marginLeft: '7px' }}> Save ₹100</span>
-          </p>
-          <p style={{ margin: '0' }}>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>₹300</span> 
-            <span style={{ color: 'gray', marginLeft: '7px', fontWeight: 'bold' }}>with</span> 
-            <img className='logo' src="https://static1.hkrtcdn.com/mbnext/static/media/loyality/Union.svg" alt=""  />
-            <span style={{ color: '#ffbe00', fontWeight: 'bolder' }}> Premium</span>
-          </p>
-          <button className='btn'><b>Add to Cart</b></button>
-        </div>
-
-        {/* Repeat the same for other cards */}
-        <div className='card'>
-          <img src="https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_c_m.jpg" alt="" />
-          <p style={{ margin: '0' }}>Biozyme Performance Whey</p>
-          <h4 style={{ margin: '0' }}>4.4 lb Rich chocolate</h4>
-          <p style={{ margin: '0' }}>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>₹400</span> 
-            <span style={{ textDecoration: 'line-through', color: 'gray', marginLeft: '7px' }}>₹500</span> 
-            <span style={{ color: 'green', fontSize: 'small', marginLeft: '7px' }}> Save ₹100</span>
-          </p>
-          <p style={{ margin: '0' }}>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>₹300</span> 
-            <span style={{ color: 'gray', marginLeft: '7px', fontWeight: 'bold' }}>with</span> 
-            <img className='logo' src="https://static1.hkrtcdn.com/mbnext/static/media/loyality/Union.svg" alt=""  />
-            <span style={{ color: '#ffbe00', fontWeight: 'bolder' }}> Premium</span>
-          </p>
-          <button className='btn'><b>Add to Cart</b></button>
-        </div>
-
-        <div className='card'>
-          <img src="https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_c_m.jpg" alt="" />
-          <p style={{ margin: '0' }}>Biozyme Performance Whey</p>
-          <h4 style={{ margin: '0' }}>4.4 lb Rich chocolate</h4>
-          <p style={{ margin: '0' }}>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>₹400</span> 
-            <span style={{ textDecoration: 'line-through', color: 'gray', marginLeft: '7px' }}>₹500</span> 
-            <span style={{ color: 'green', fontSize: 'small', marginLeft: '7px' }}> Save ₹100</span>
-          </p>
-          <p style={{ margin: '0' }}>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>₹300</span> 
-            <span style={{ color: 'gray', marginLeft: '7px', fontWeight: 'bold' }}>with</span> 
-            <img className='logo' src="https://static1.hkrtcdn.com/mbnext/static/media/loyality/Union.svg" alt=""  />
-            <span style={{ color: '#ffbe00', fontWeight: 'bolder' }}> Premium</span>
-          </p>
-          <button className='btn'><b>Add to Cart</b></button>
-        </div>
+  const renderCards = () => {
+    return cardData.map((card, index) => (
+      <div className='card-i' key={index}>
+        <img className="image" src={card.image} alt="" />
+        <p className="span" style={{ margin: '0' }}>{card.title}</p>
+        <h4 className="span" style={{ margin: '0' }}>{card.description}</h4>
+        <p className="span" style={{ margin: '0' }}>
+          <span style={{ color: 'black', fontWeight: 'bold' }}>₹{card.price}</span> 
+          <span style={{ textDecoration: 'line-through', color: 'gray', marginLeft: '7px' }}>₹{card.originalPrice}</span> 
+          <span style={{ color: 'green', fontSize: 'small', marginLeft: '7px' }}> Save ₹{card.discount}</span>
+        </p>
+        <p className="span" style={{ margin: '0' }}>
+          <span style={{ color: 'black', fontWeight: 'bold' }}>₹300</span> 
+          <span style={{ color: 'gray', marginLeft: '7px', fontWeight: 'bold' }}>with</span> 
+          <img className='logo-s' src="https://static1.hkrtcdn.com/mbnext/static/media/loyality/Union.svg" alt=""  />
+          <span style={{ color: '#ffbe00', fontWeight: 'bolder' }}> Premium</span>
+        </p>
+        <button className='btn'><b>ADD TO CART</b></button>
       </div>
+    ));
+  };
+
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true
+  };
+
+  return (
+    <div className="container">
+      <div className="cards">
+      <div className="slider-container">
+        <Slider {...settings}>
+          {renderCards()}
+        </Slider>
+        </div>
       </div>
     </div>
   );
 }
-
-export default Cards;
