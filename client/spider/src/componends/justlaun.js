35c6d40f33ced987d2a71225915b434eac6b1427
Neha Dhok <nehadhok306@gmail.com>
2024-05-17 08:32:42 +0530

    .

---
+++ client/spider/src/componends/justlaun.js
@@ -0,0 +1,39 @@
+const cardData1 = [
+    {
+      image: "https://img10.hkrtcdn.com/32565/prd_3256409-MuscleBlaze-Biozyme-Whey-PR-2.2-lb-Molten-Chocolate-Cake_c_m.jpg",
+      title: "MuscleBlaze Creatine Monohydrate CreAMP & PRE Workout 200 Xtreme 0.22 lb Combo",
+      description: "",
+      price: 919,
+      originalPrice: 1398,
+      discount: 479
+    },
+    {
+        image: "https://img8.hkrtcdn.com/32441/prd_3244007-MuscleBlaze-Creatine-Monohydrate-CreAMP-Citrus-Blast-0.55-lb_c_m.jpg",
+        title: "MuscleBlaze T-Surge Black, 90 tablet(s)",
+        description: "90 tablet(s)  Unflavoured ",
+        price: 2649,
+        originalPrice: 3499,
+        discount: 850
+      },{
+        image: "https://img4.hkrtcdn.com/32441/prd_3244033-MuscleBlaze-Pre-Workout-CreaXtreme-0.44-lb-Berry-Lime_c_m.jpg",
+        title: "Koshaveda Shilajit Pro by MuscleBlaze 20g & Shaker Combo",
+        description: "60 tablet(s)  Unflavoured  ",
+        price: 899,
+        originalPrice:1299 ,
+        discount: 400
+      },{
+        image: "https://img4.hkrtcdn.com/33099/prd_3309843_c_m.jpg",
+        description: "0.88 lb Unflavoured",
+        price:1399 ,
+        originalPrice: 1799,
+        discount:400
+      },
+        image: "",
+        description: "0.88 lb Unflavoured",
+        price: ,
+        originalPrice: ,
+        discount:
+      }
+
+  ]
+  
---
+++ client/spider/src/componends/trending.js
@@ -0,0 +1,32 @@
+const cardData1 = [
+  {
+    image: "https://img10.hkrtcdn.com/33802/pck_3380129_c_m.jpg",
+    title: "MuscleBlaze Creatine Monohydrate CreAMP & PRE Workout 200 Xtreme 0.22 lb Combo",
+    description: "",
+    price: 919,
+    originalPrice: 1398,
+    discount: 479
+  },
+  {
+      image: "https://img6.hkrtcdn.com/27730/prd_2772955-MuscleBlaze-TSurge-Black-90-tablets-Unflavoured_c_m.jpg",
+      title: "MuscleBlaze T-Surge Black, 90 tablet(s)",
+      description: "90 tablet(s)  Unflavoured ",
+      price: 149,
+      originalPrice: 1399,
+      discount: 450
+    },{
+      image: "https://img6.hkrtcdn.com/33236/pck_3323515_c_m.jpg",
+      title: "Koshaveda Shilajit Pro by MuscleBlaze 20g & Shaker Combo",
+      description: "60 tablet(s)  Unflavoured  ",
+      price: 1099,
+      originalPrice: 1998,
+      discount: 899
+    },{
+      image: "https://img6.hkrtcdn.com/30595/prd_3059465-MuscleBlaze-Fish-Oil-1000mg-MBVite-Multivitamin-Combo-60-tablets-Unflavoured_c_m.jpg",
+      title: "MuscleBlaze Fish Oil 1000mg & MB-Vite Multivitamin Combo, 60 tablet(s)",
+      description: "0.88 lb Unflavoured",
+      price: 899,
+      originalPrice: 1348,
+      discount: 449
+    },
+]
